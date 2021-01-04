565f8fdeb2e09d05044d9a9c206dc05067c2271f
vismael <xjfw.3.vip@gmail.com>
2021-01-04 00:27:06 -0300

    initial commit, base idea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
+
+const iconList = Object
+  .keys(Icons)
+  .filter(key => key !== "fas" && key !== "prefix" )
+  .map(icon => Icons[icon])
+
+library.add(...iconList)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        TODO
       </header>
+      <div className="Tag-header">
+        TODO
+      </div>
+      <div className="Content">
+
+        <div className="card">
+          <div className="card-title">
+            <div className="card-title-text">
+              Understand CSS Mask in 4 Minutes
+            </div>
+            <div className="price">
+              $46.334
+            </div>
+          </div>
+          <div className="card-footer">
+            <div className="tags">
+              <div className="tag"> Comida </div>
+              <div className="tag"> Salud</div>
+              <div className="tag"> Regalos</div>
+            </div>
+          </div>
+        </div>
+        </div>
+
     </div>
   );
 }
